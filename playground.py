ed2facf680535536d0ded895af03f99df1a19fb5
octopusthu <octopusthu@gmail.com>
2023-08-14 11:59:14 +0800

    Updates

---
+++ playground.py
@@ -0,0 +1,14 @@
+# @brief Playground
+#
+# @author octopusthu
+#
+
+
+def main():
+    # Pass None as format() argument
+    print("Hello, {}!".format(None))
+
+
+# main
+if __name__ == "__main__":
+    main()
